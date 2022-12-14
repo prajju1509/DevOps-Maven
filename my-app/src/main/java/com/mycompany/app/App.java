64cb692f0e3087bfe038225326fd21d2201a3ab8
Prajwal BM <117504878+prajju1509@users.noreply.github.com>
2022-12-14 18:34:40 +0530

    Update App.java

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hi. people, welcome to my maven project!" );
+        System.out.println( "Hi. people, welcome to prajwal's maven project!" );
     }
 }
